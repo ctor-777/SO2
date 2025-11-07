a44cb261de9b1075e9d0f18c0088e9d5726742b7
Ferrann999 <ferrann999@gmail.com>
2025-11-07 22:31:28 +0100

    Add parent pointer and child lists to task_struct

--- include/sched.h
+++ include/sched.h
@@ -26,6 +26,10 @@ struct task_struct {
 	enum state_t state;
 	unsigned int quantum;
 	page_table_entry * dir_pages_baseAddr;
+	struct task_struct parent*		//Puntero al padre
+	struct list_head child_anchor;		//list_head para la lista de hijos del padre
+	struct list_head childs;		//Lista de hijos de este proceso
+	int pending_unblocks;
 };
 
 union task_union {
